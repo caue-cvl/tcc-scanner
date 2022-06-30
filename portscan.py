0acb6c2e9fe916efa3bd086752a379aa2285c7e7
Cauê Cavaliere <caue.cavaliere@gmail.com>
2022-06-30 20:27:47 -0300

    Ciração de array com resultado do scan para ser feito a pesquisa

--- portscan.py
+++ portscan.py
@@ -1,44 +1,92 @@
+#IMPORTAÇÃO DE BIBLIOTECA
+
 import nmap 
-   
-begin = 78
-end = 80
-target = '192.168.0.26'
-   
-scanner = nmap.PortScanner() 
+
+#DECLARAÇÃO DE FUNÇÕES
 
 def banner():
     print("""
-  _______ _____ _____            ______   _______ ______ _____ 
- |__   __/ ____/ ____|          |  ____/\|__   __|  ____/ ____|
-    | | | |   | |       ______  | |__ /  \  | |  | |__ | |     
-    | | | |   | |      |______| |  __/ /\ \ | |  |  __|| |     
-    | | | |___| |____           | | / ____ \| |  | |___| |____ 
-    |_|  \_____\_____|          |_|/_/    \_\_|  |______\_____|
-                                                                          
-                                                                          """)
+      _______ _____ _____            ______   _______ ______ _____ 
+     |__   __/ ____/ ____|          |  ____/\|__   __|  ____/ ____|
+        | | | |   | |       ______  | |__ /  \  | |  | |__ | |     
+        | | | |   | |      |______| |  __/ /\ \ | |  |  __|| |     
+        | | | |___| |____           | | / ____ \| |  | |___| |____ 
+        |_|  \_____\_____|          |_|/_/    \_\_|  |______\_____|
+                                                                            
+    """)
 
-banner()
+def porta_fechada():
+    print(f'Porta {i} está {resultado_estado_conexao}.')
+def porta_aberta():
+    print(f'Porta {i} está {resultado_estado_conexao}, com o serviço {resultado_servico_conexao} do produto {resultado_produto_conexao} na versão {resultado_versao_conexao}.')
+    query_pesquisa_cve = resultado_servico_conexao + ' ' + resultado_produto_conexao + ' ' + resultado_versao_conexao + ' cve'
+    arr_query_pesquisa_cve.append(query_pesquisa_cve)
+def porta_aberta_vesao_nao_identificada():
+    print(f'Porta {i} está {resultado_estado_conexao}, com o serviço {resultado_servico_conexao} do produto {resultado_produto_conexao}. (Versão não identificada).')
+    query_pesquisa_cve = resultado_servico_conexao + ' ' + resultado_produto_conexao + ' cve'
+    arr_query_pesquisa_cve.append(query_pesquisa_cve)
 
-verbose = input('Você deseja fazer a pesquisa em verbose mode?(S/N): ')
+#DECLARAÇÃO DE VARIAVÉIS
+
+inicio_escopo_scan = 20 
+fim_escopo_scan = 23
+alvo = '192.168.0.26'
+
+resultado = ''
+resultado_estado_conexao = ''
+resultado_servico_conexao = ''
+resultado_produto_conexao = ''
+resultado_versao_conexao = ''
+resposta_opcao_verbose = 0
+verbose = ''
+query_pesquisa_cve = ''
+arr_query_pesquisa_cve = []
+
+# INICIO DO SCRIPT   
+scanner = nmap.PortScanner() # EXECUÇÃO DE SCAN
+
+banner() # APARECER BANNER (TCC- FATEC)
+
+verbose = input('Você deseja fazer a pesquisa em modo verbose (modo detalhado)? (S/N): ') # PEDIR SE O USUÁRIO QUER SABER AS PORTAS FECHADAS OU NÃO
 print()
 
-for i in range(begin,end+1):
-
-    resu = scanner.scan(target,str(i),arguments='-sV')
-    res_state = resu['scan'][target]['tcp'][i]['state']
-    res_name = resu['scan'][target]['tcp'][i]['name']
-    res_product = resu['scan'][target]['tcp'][i]['product']
-    res_version = resu['scan'][target]['tcp'][i]['version']
-
+while resposta_opcao_verbose == 0: # INICIO LAÇO RESPOSTA VERBOSE
     if verbose == 'S' or verbose == 's':
-        if res_state == 'closed':
-            print(f'Port {i} is {res_state}.')
-        else:
-            print(f'Port {i} is {res_state}, with {res_name} protocol in {res_product} at {res_version}.')
+        resposta_opcao_verbose += 1
     elif verbose == 'N' or verbose == 'n':
-        if res_state == 'closed':
-            continue
-        else:
-            print(f'Port {i} is {res_state}, with {res_name} protocol in {res_product} at {res_version}.')
+        resposta_opcao_verbose += 1
     else:
-        continue
+        verbose = input('Você deseja fazer a pesquisa em modo verbose (modo detalhado)? (S/N): ') # PEDIR SE O USUÁRIO QUER SABER AS PORTAS FECHADAS OU NÃO
+        print()
+
+for i in range(inicio_escopo_scan,fim_escopo_scan+1): # INICIO LAÇO SCAN
+
+    resultado = scanner.scan(alvo,str(i),arguments='-sV') # USO DO PARAMETRO -sV DA FERRAMENTA NMAP
+    resultado_estado_conexao = resultado['scan'][alvo]['tcp'][i]['state'] # COLETA ESTADO DA PORTA (ABERTA OU FECHADA)
+    resultado_servico_conexao = resultado['scan'][alvo]['tcp'][i]['name'] # COLETA SERVIÇO (HTTP, SSH, SFTP, FTP)
+    resultado_produto_conexao = resultado['scan'][alvo]['tcp'][i]['product'] # COLETA DO PRODUTO (APACHE HTTPD, OPENSSH)
+    resultado_versao_conexao = resultado['scan'][alvo]['tcp'][i]['version'] # COLETA VERSÃO DO PRODUTO
+    
+    if resultado_estado_conexao == 'closed': # TRADUÇÃO DO RESULTADO, A BIBLIOTECA TRÁS O RESULTADO EM INGLÊS
+        resultado_estado_conexao = 'fechada' # TRADUÇÃO EM CASO DE PORTA FECHADA
+    else:
+        resultado_estado_conexao = 'aberta' # TRADUÇÃO EM CASO DE PORTA ABERTA
+
+    if verbose == 'S' or verbose == 's': # CASO ESCOLHA POSITIVAMENTE NA VARIÁVEL VERBOSE
+        if resultado_estado_conexao == 'fechada': # CASO ENCONTRE PORTA FECHADA MOSTRE O RESULTADO
+            porta_fechada()
+        else: # CASO ENCONTRE PORTA ABERTA MOSTRE O RESULTADO
+            if resultado_versao_conexao != '':
+                porta_aberta()
+            else:
+                porta_aberta_vesao_nao_identificada()
+    elif verbose == 'N' or verbose == 'n': # CASO ESCOLHA NEGATIVAMENTE NA VARIÁVEL VERBOSE
+        if resultado_estado_conexao == 'fechada': # CASO ENCONTRE PORTA FECHADA PASSE PARA PRÓXIMO ITEM DO LAÇO
+            continue 
+        else: # CASO ENCONTRE PORTA ABERTA MOSTRE O RESULTADO
+            if resultado_versao_conexao != '':
+                porta_aberta()
+            else:
+                porta_aberta_vesao_nao_identificada()
+    else: # CASO COLOQUE ALGO DIFERENTE DE (S/s) OU (N/n) PROSSIGA PARA PRÓXIMO ITEM DO LAÇO
+        continue 
