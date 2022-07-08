144df028257a5af289a7bd36d2448d9e07b6a709
Cauê Cavaliere <caue.cavaliere@gmail.com>
2022-07-08 19:04:34 -0300

    Refatorando parte pesquisa base

--- search_google.py
+++ search_google.py
@@ -19,15 +19,11 @@ REVERSE = "\033[;7m"
 
 
 #variaveis
-total_results_crit = 0
-total_results_high = 0
-total_results_medium = 0
-total_results_low = 0
 
 cve_id_crit = []
 cve_id_high = []
-cve_id_medium = []
 cve_id_low = []
+cve_id_medium = []
 
 severity_crit_list = []
 severity_high_list = []
@@ -37,73 +33,43 @@ severity_low_list = []
 def get_all_cves_available_for_search(search):
     req = requests.get(RETRIEVE_A_COLLECTION_FROM_CVE, search)
     json_response = json.loads(req.content.decode())
-    set_total_results_variables(json_response)
-    get_cve_according_severity(json_response)
-
-
-def set_total_results_variables(json_available):
-    global total_results_low
-    global total_results_medium
-    global total_results_high
-    global total_results_crit
-
-    cves_available = json_available['result']['CVE_Items']
-    for cve in cves_available:
-        match cve['impact']['baseMetricV2']['severity']:
-            case 'LOW':
-                total_results_low+=1
-            case 'MEDIUM':
-                total_results_medium+=1
-            case 'HIGH':
-                total_results_high+=1
-            case 'CRITICAL':
-                total_results_crit+=1
-
+    return get_cve_according_severity(json_response)
 
 def get_cve_according_severity(json_available):
-    global cve_id_crit
-    global cve_id_high
-    global cve_id_medium
-    global cve_id_low
-
+    
     cves_available = json_available['result']['CVE_Items']
-    critical_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'CRITICAL']
-    high_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'HIGH']
-    medium_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'MEDIUM']
-    low_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'LOW']
+    cve_id_collection = {'CRITICAL':[],'HIGH':[],'MEDIUM':[],'LOW':[]}
 
-    for cve in low_cves:
-        cve_id_low.append(cve['cve']['CVE_data_meta']['ID'])
-    for cve in medium_cves:
-        cve_id_medium.append(cve['cve']['CVE_data_meta']['ID'])
-    for cve in high_cves:
-        cve_id_high.append(cve['cve']['CVE_data_meta']['ID'])
-    for cve in critical_cves:
-        cve_id_crit.append(cve['cve']['CVE_data_meta']['ID'])
+    for cve in cves_available:
+        if cve['impact']['baseMetricV2']['severity'] == 'CRITICAL':
+            cve_id_collection['CRITICAL'].append(cve['cve']['CVE_data_meta']['ID'])
+        if cve['impact']['baseMetricV2']['severity'] == 'HIGH':
+            cve_id_collection['HIGH'].append(cve['cve']['CVE_data_meta']['ID'])
+        if cve['impact']['baseMetricV2']['severity'] == 'MEDIUM':
+            cve_id_collection['MEDIUM'].append(cve['cve']['CVE_data_meta']['ID'])
+        if cve['impact']['baseMetricV2']['severity'] == 'LOW':
+            cve_id_collection['LOW'].append(cve['cve']['CVE_data_meta']['ID'])
 
     get_severity_points(json_available)
+    return cve_id_collection
 
 
 def get_severity_points(json_available):
-    global severity_crit_list
-    global severity_high_list
-    global severity_medium_list
-    global severity_low_list
 
     cves_available = json_available['result']['CVE_Items']
-    critical_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'CRITICAL']
-    high_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'HIGH']
-    medium_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'MEDIUM']
-    low_cves = [cve for cve in cves_available if cve['impact']['baseMetricV2']['severity'] == 'LOW']
+    severity_collection = {'CRITICAL':[],'HIGH':[],'MEDIUM':[],'LOW':[]}
 
-    for cve in low_cves:
-        severity_low_list.append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
-    for cve in medium_cves:
-        severity_medium_list.append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
-    for cve in high_cves:
-        severity_high_list.append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
-    for cve in critical_cves:
-        severity_crit_list.append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
+    for cve in cves_available:
+        if cve['impact']['baseMetricV2']['severity'] == 'CRITICAL':
+            severity_collection['CRITICAL'].append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
+        if cve['impact']['baseMetricV2']['severity'] == 'HIGH':
+            severity_collection['HIGH'].append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
+        if cve['impact']['baseMetricV2']['severity'] == 'MEDIUM':
+            severity_collection['MEDIUM'].append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
+        if cve['impact']['baseMetricV2']['severity'] == 'LOW':
+            cve_id_collection['LOW'].append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
+    print(severity_collection)
+    return severity_collection
 
 
 def print_cve_id_and_severity(color, text, cve_id_list, severity_arr):
@@ -114,26 +80,30 @@ def print_cve_id_and_severity(color, text, cve_id_list, severity_arr):
             print(f'      - REFERENCE: https://nvd.nist.gov/vuln/detail/{cve_id}')
         print('------------------------------------------')
 
+def print_result():
+    services_availables = portscan.sequencia_execucao(20, 23)
+    print(services_availables)
 
-services_availables = portscan.sequencia_execucao(20, 23)
-print(services_availables)
-for service in services_availables:
+    global severity_crit_list
+    global severity_high_list
+    global severity_medium_list
+    global severity_low_list
     
-    severity_crit_list = []
-    severity_high_list = []
-    severity_medium_list = []
-    severity_low_list = []
-    cve_id_crit = []
-    cve_id_high = []
-    cve_id_medium = []
-    cve_id_low = []
 
-    keyword_to_search = { 'keyword': service }
-    get_all_cves_available_for_search(keyword_to_search)
-    try:        
-        print_cve_id_and_severity(PURPLE, "CRITICAL", cve_id_crit, severity_crit_list)
-        print_cve_id_and_severity(RED, "HIGH", cve_id_high, severity_high_list)
-        print_cve_id_and_severity(YELLOW, "MEDIUM", cve_id_medium, severity_medium_list)
-        print_cve_id_and_severity(CYAN, "LOW", cve_id_low, severity_low_list)
-    except IndexError:
-        print('ERRO NO INDEX')
+    for service in services_availables:
+        
+        severity_crit_list = []
+        severity_high_list = []
+        severity_medium_list = []
+        severity_low_list = []
+        keyword_to_search = { 'keyword': service }
+        severity_list = get_severity_points(keyword_to_search)
+        cve_id_list = get_all_cves_available_for_search(keyword_to_search)  
+        print_cve_id_and_severity(PURPLE, "CRITICAL", cve_id_list['CRITICAL'], severity_list['CRITICAL'])
+        print_cve_id_and_severity(RED, "HIGH", cve_id_list['HIGH'], severity_list['HIGH'])
+        print_cve_id_and_severity(YELLOW, "MEDIUM", cve_id_list['MEDIUM'], severity_list['MEDIUM'])
+        print_cve_id_and_severity(CYAN, "LOW", cve_id_list['LOW'], severity_list['LOW'])
+    return()
+    
+
+print_result()
