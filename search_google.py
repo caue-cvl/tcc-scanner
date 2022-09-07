ff0fa1e9369fbf90a209451630024f96929acc05
Bbeluco <beluco.bispo@hotmail.com>
2022-09-06 22:49:58 -0300

    codigo com validacao de base de impactos adicionada

--- search_google.py
+++ search_google.py
@@ -30,20 +30,40 @@ def coletar_informacoes_especificas_from_endpoint_response(json_available, filtr
 def catalogar_informacoes_por_severidade(cves_disponiveis, filtros_informacoes):
     severidades = {'CRITICAL':[],'HIGH':[],'MEDIUM':[],'LOW':[]}
     for cve in cves_disponiveis:
-        match cve['impact']['baseMetricV2']['severity']:
-            case 'CRITICAL':
-                popular_informacao_desejada(severidades['CRITICAL'], cve, filtros_informacoes)
-            case 'HIGH':
-                popular_informacao_desejada(severidades['HIGH'], cve, filtros_informacoes)
-            case 'MEDIUM':
-                popular_informacao_desejada(severidades['MEDIUM'], cve, filtros_informacoes)
-            case 'LOW':
-                popular_informacao_desejada(severidades['LOW'], cve, filtros_informacoes)
+        if is_cve_homol(cve):
+            match get_severity_text(cve):
+                case 'CRITICAL':
+                    popular_informacao_desejada(severidades['CRITICAL'], cve, filtros_informacoes)
+                case 'HIGH':
+                    popular_informacao_desejada(severidades['HIGH'], cve, filtros_informacoes)
+                case 'MEDIUM':
+                    popular_informacao_desejada(severidades['MEDIUM'], cve, filtros_informacoes)
+                case 'LOW':
+                    popular_informacao_desejada(severidades['LOW'], cve, filtros_informacoes)
 
     return severidades
 
+def is_cve_homol(cve):
+    if cve['impact'] == {}:
+          return False
+    return True
+
+def get_severity_text(cve):
+    if 'baseMetricV2' in cve['impact']:
+        return cve['impact']['baseMetricV2']['severity']
+    else:
+        return cve['impact']['baseMetricV3']['cvssV3']['baseSeverity']
+    
+
 def popular_informacao_desejada(severidade, cve, filtros_informacoes):
     informacao_especifica = cve
+    if 'baseMetric' in filtros_informacoes:
+        if 'baseMetricV2' in cve['impact']:
+            severidade.append(cve['impact']['baseMetricV2']['cvssV2']['baseScore'])
+        else:
+            severidade.append(cve['impact']['baseMetricV3']['cvssV3']['baseScore'])
+        return
+
     for informacao in filtros_informacoes:
         informacao_especifica = informacao_especifica.get(informacao)
     severidade.append(informacao_especifica)
@@ -66,9 +86,8 @@ def print_result():
         keyword_to_search = { 'keyword': service, 'apiKey': api_key }
         json_response_from_search_endpoint = query_endpoint_to_get_cve(keyword_to_search)
         cve_id_list = coletar_informacoes_especificas_from_endpoint_response(json_response_from_search_endpoint, ['cve', 'CVE_data_meta', 'ID'])
-        severity_list = coletar_informacoes_especificas_from_endpoint_response(json_response_from_search_endpoint, ['impact', 'baseMetricV2', 'cvssV2', 'baseScore'])
+        severity_list = coletar_informacoes_especificas_from_endpoint_response(json_response_from_search_endpoint, ['impact', 'baseMetric', 'cvssV2', 'baseScore'])
         get_cves_founds_in_search_endpoint(cve_id_list, severity_list, index)
-        ###VO MEXE
         teste_json = ainda_nao_sei_o_nome(index, service, json_response_from_search_endpoint)
     
     with open('data.json', 'w') as f:
@@ -101,19 +120,26 @@ def ainda_nao_sei_o_nome(index, service, response):
     json_final[f"PORTA {portscan.portas[index]}"] = []
     json_aux = {}
     for item in response['result']['CVE_Items']:
-        json_aux["SERVICO"] = service.split()[0]
-        json_aux["PRODUTO"] = service.split()[1]
-        json_aux["CVE-ID"] = item['cve']['CVE_data_meta']['ID']
-        json_aux["BASE-SCORE"] =  item['impact']['baseMetricV2']['cvssV2']['baseScore']
-        json_aux["DESCRICAO"] = item['cve']['description']['description_data']
-        ref_total = []
-        for ref in item['cve']['references']['reference_data']:
-            ref_total.append(ref['url'])
+        if item['impact'] != {}:
+            json_aux["SERVICO"] = service.split()[0]
+            if len(service.split()) == 2:
+                json_aux["PRODUTO"] = service.split()[1]
+            else:
+                json_aux["PRODUTO"] = ""
+            json_aux["CVE-ID"] = item['cve']['CVE_data_meta']['ID']
+            if 'baseMetricV2' in item['impact']:
+                json_aux["BASE-SCORE"] =  item['impact']['baseMetricV2']['cvssV2']['baseScore']
+            elif 'baseMetricV3' in item['impact']:
+                json_aux["BASE-SCORE"] =  item['impact']['baseMetricV3']['cvssV3']['baseScore']
+            json_aux["DESCRICAO"] = item['cve']['description']['description_data']
+            ref_total = []
+            for ref in item['cve']['references']['reference_data']:
+                ref_total.append(ref['url'])
 
-        json_aux["REFERENCIAS"] = ref_total
+            json_aux["REFERENCIAS"] = ref_total
 
-        json_final[f"PORTA {portscan.portas[index]}"].append(json_aux)
-        json_aux = {}
+            json_final[f"PORTA {portscan.portas[index]}"].append(json_aux)
+            json_aux = {}
 
     return json_final
 
